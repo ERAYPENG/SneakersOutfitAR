05878cb81d9687a731fa44e5bffd4ebcb798ccff
ERAYPENG <jameslebron23@hotmail.com.tw>
2021-02-23 23:30:24 +0800

    set up UI

---
+++ SneakersOutfitAR/Common/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SneakersOutfitAR
+//
+//  Created by ERAY on 2021/2/19.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UISceneDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = LogInViewController()
+        vc.view.backgroundColor = .systemGroupedBackground
+        self.window?.rootViewController = vc
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
+
